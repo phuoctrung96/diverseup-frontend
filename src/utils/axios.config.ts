e32413687ecfccefff281e91b609a3e9681d533e
ledutu <ledutu.it@gmail.com>
2022-04-24 02:16:26 +0700

    axios config

---
+++ src/utils/axios.config.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import AuthHelpers from './AuthHelpers';
+
+export const baseURL = 'https://backend.diverseup.com';
+
+// Set config defaults when creating the instance
+const instance = axios.create({
+  baseURL,
+});
+
+instance.interceptors.request.use(
+  function (config: any) {
+    const TOKEN = AuthHelpers.getAccessToken();
+    if (TOKEN) {
+      config.headers.Authorization = `Bearer ${TOKEN}`;
+    }
+    console.log({ config });
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    console.log({ response });
+    return response?.data;
+  },
+  function (error) {
+    console.log(error.response);
+    return Promise.reject(error?.response?.data);
+  }
+);
+
+export default instance;
