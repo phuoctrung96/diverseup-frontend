6c7c37f552f920ad0279d5ced79953bbab3d452d
ledutu <ledutu.it@gmail.com>
2022-04-24 15:48:43 +0700

    inragrate api

--- src/api/user.ts
+++ src/api/user.ts
@@ -30,26 +30,73 @@ export const loginApi = (body: ILoginBody): Promise<ILoginRes> => {
   });
 };
 
-export const logout = () => {
+export const logoutApi = () => {
   return axios.post('/api/auth/jwt/logout');
 };
 
-export interface IGetGoogleAuthorizeRes {
-  authorization_url: string;
+export interface IForgotPasswordBody {
+  email: string;
 }
-export const getGoogleAuthorize = async () => {
-  const res: IGetGoogleAuthorizeRes = await axios.get('/api/auth/google/authorize');
-  return res;
+export const forgotPasswordApi = (body: IForgotPasswordBody) => {
+  return axios.post('/api/auth/forgot-password', body);
 };
 
-interface IGetGoogleCallbackReq {
+export interface IResetPasswordBody {
+  token: string;
+  password: string;
+}
+export const resetPasswordApi = (body: IResetPasswordBody) => {
+  return axios.post('/api/auth/reset-password', body);
+};
+
+export interface IRequestVerifyTokenBody {
+  email: string;
+}
+export const requestVerifyTokenApi = (body: IRequestVerifyTokenBody) => {
+  return axios.post('/api/auth/request-verify-token', body);
+};
+
+export interface IVerifyBody {
+  token: string;
+}
+export const verifyApi = (body: IVerifyBody) => {
+  return axios.post('/api/auth/verify', body);
+};
+
+export interface IGoogleAuthorizeRes {
+  authorization_url: string;
+}
+export const googleAuthorizeApi = (): Promise<IGoogleAuthorizeRes> => {
+  return axios.get('/api/auth/google/authorize');
+};
+
+interface IGoogleCallbackParams {
   code: string;
   code_verifier: string;
   state: string;
   error: string;
 }
-export const getGoogleCallback = (params: IGetGoogleCallbackReq) => {
+export const googleCallbackApi = (params: IGoogleCallbackParams) => {
   return axios.get('/api/auth/google/callback', {
+    params,
+  });
+};
+
+export interface ILinkedinAuthorizeRes {
+  authorization_url: string;
+}
+export const linkedinAuthorizeApi = (): Promise<ILinkedinAuthorizeRes> => {
+  return axios.get('/api/auth/linkedin/authorize');
+};
+
+interface ILinkedinCallbackParams {
+  code: string;
+  code_verifier: string;
+  state: string;
+  error: string;
+}
+export const linkedinCallbackApi = (params: ILinkedinCallbackParams) => {
+  return axios.get('/api/auth/linkedin/callback', {
     params,
   });
 };
